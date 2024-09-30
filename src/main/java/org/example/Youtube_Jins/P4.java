375a90775dbcee559d60d68b52f57f04fa531575
Kjo <krt8599@naver.com>
2024-09-30 23:13:02 +0900

    자기전 커밋 오늘도 잘 자요 고생했어요

--- src/main/java/org/example/Youtube_Jins/P4.java
+++ src/main/java/org/example/Youtube_Jins/P4.java
@@ -8,13 +8,21 @@ public class P4 {
         System.out.println("수를 입력해 주세요");
         int n = sc.nextInt();
         System.out.println("입력받은수 \t" +n);
-    }//main
-    static int twojin(int n){
-        while(n>0){
-            int na = n % 2;
-            n=n/2;
-            return na;
+
+        int mok =n;
+        int index = 0;
+        int[] arr = new int[100];
+        while (mok > 0) {
+            arr[index] = mok % 2;
+            mok = mok / 2;
+            index++;
         }
 
-    }
+
+        for(int x=index-1;x>=0;x--){
+            System.out.print(arr[x]);
+        }
+        sc.close();
+    }//main
+
 }

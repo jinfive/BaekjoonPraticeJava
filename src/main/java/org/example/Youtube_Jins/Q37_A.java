5c6007cae91cbd0250616a6566e3d34b8fa731c3
Kjo <krt8599@naver.com>
2024-11-10 21:50:30 +0900

    단지수 측정 문제

--- src/main/java/org/example/Youtube_Jins/Q37_A.java
+++ src/main/java/org/example/Youtube_Jins/Q37_A.java
@@ -2,22 +2,52 @@ package org.example.Youtube_Jins;
 
 
 public class Q37_A {
-    public static int arr[][] = {
-            {0,0,0,1,1},
-            {0,0,0,0,1},
-            {1,0,0,0,0},
-            {1,1,0,0,0},
-            {1,1,0,1,1}};
+    static final int SIZE = 5;
+    static final int APART = 1;
 
-    final static int SIZE = arr.length;
+    static int apartCnt = 0;
+    static int []houseHold = new int[10];
 
-    public static void apt(int y,int x){
-        if(arr[y][x]==1){
-            
-        }
+    static int [][]map =
+            {{0,0,0,1,1},
+                    {0,0,0,0,1},
+                    {1,0,0,0,0},
+                    {1,1,0,0,0},
+                    {1,1,0,1,1}};
+
+
+    public static void doFunc(int y, int x, int apartIndex) {
+        // Change color to 0
+        map[y][x] = 0;
+        houseHold[apartIndex]++;
+
+        if (x - 1 >= 0 && map[y][x-1] == APART)
+            doFunc(y, x-1, apartIndex);
+        if (x + 1 <= SIZE-1 && map[y][x+1] == APART)
+            doFunc(y, x+1, apartIndex);
+        if (y + 1 <= SIZE-1 && map[y+1][x] == APART)
+            doFunc(y+1, x, apartIndex);
+        if (y - 1 >= 0 && map[y-1][x] == APART)
+            doFunc(y-1, x, apartIndex);
     }
 
     public static void main(String[] args) {
 
+        for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++){
+                if (map[i][j] == 1) {
+                    doFunc(i, j, apartCnt);
+                    apartCnt++;
+                }
+            }
+        }
+
+        System.out.println(apartCnt);
+        for(int i : houseHold) {
+            System.out.print(i + " ");
+        }
     }
+
+
+
 }

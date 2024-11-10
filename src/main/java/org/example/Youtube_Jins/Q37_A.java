a01a1e3bca284c484416351f657f5a26fc575026
Kjo <krt8599@naver.com>
2024-11-10 20:54:23 +0900

    단지수 구하기 문제

---
+++ src/main/java/org/example/Youtube_Jins/Q37_A.java
@@ -0,0 +1,23 @@
+package org.example.Youtube_Jins;
+
+
+public class Q37_A {
+    public static int arr[][] = {
+            {0,0,0,1,1},
+            {0,0,0,0,1},
+            {1,0,0,0,0},
+            {1,1,0,0,0},
+            {1,1,0,1,1}};
+
+    final static int SIZE = arr.length;
+
+    public static void apt(int y,int x){
+        if(arr[y][x]==1){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

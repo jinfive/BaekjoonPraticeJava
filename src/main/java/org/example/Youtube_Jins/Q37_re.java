e052f4461b4fc3d6a00b9dc16d240cbf01690abf
Kjo <krt8599@naver.com>
2024-11-11 20:31:03 +0900

    다시한번 코드 작성해보자~

---
+++ src/main/java/org/example/Youtube_Jins/Q37_re.java
@@ -0,0 +1,12 @@
+package org.example.Youtube_Jins;
+
+public class Q37_re {
+
+        public static int arr[][] = {
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0},
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1}};
+
+    }

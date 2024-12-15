346eb911dbbfa682c0e1d5e264067b90d068c5e6
Kjo <krt8599@naver.com>
2024-12-15 19:48:27 +0900

    문풀 준비

--- src/main/java/org/example/Youtube_Jins/Q44_A.java
+++ src/main/java/org/example/Youtube_Jins/Q44_A.java
@@ -1,9 +1,22 @@
 package org.example.Youtube_Jins;
 
 public class Q44_A {
-/*
-    1  5  2  1  4  3  4  5  2  1
-dpH 1  2  2  1  3  3  4  5  2  1
-dpl  1  5  2  1  4  3  3  3  2  1
- */
+    /*
+    백준11054
+    예제 입력 1
+    10
+    1 5 2 1 4 3 4 5 2 1
+    예제 출력 1
+    7
+    힌트
+    예제의 경우 {1 5 2 1 4 3 4 5 2 1}이 가장 긴 바이토닉 부분 수열이다.
+        1  5  2  1  4  3  4  5  2  1
+    dpH 1  2  2  1  3  3  4  5  2  1
+    dpl  1  5  2  1  4  3  3  3  2  1
+    sum  2  7  4  2  7  6  7  8  4  2
+     */
+    public static void main(String[] args) {
+    
+    }
+
 }

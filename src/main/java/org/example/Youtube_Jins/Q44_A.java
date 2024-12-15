da2d0e51a878d6798c9a44257016da81908ba927
Kjo <krt8599@naver.com>
2024-12-15 21:23:07 +0900

    진쓰 마지막 문제 커밋

--- src/main/java/org/example/Youtube_Jins/Q44_A.java
+++ src/main/java/org/example/Youtube_Jins/Q44_A.java
@@ -1,5 +1,7 @@
 package org.example.Youtube_Jins;
 
+import java.util.Scanner;
+
 public class Q44_A {
     /*
     백준11054
@@ -15,8 +17,51 @@ public class Q44_A {
     dpl  1  5  2  1  4  3  3  3  2  1
     sum  2  7  4  2  7  6  7  8  4  2
      */
-    public static void main(String[] args) {
-    
+    public static int getMax(int a , int b){
+        if(a>b)
+            return a;
+        else
+            return b;
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요");
+        int cnt = sc.nextInt();
+        int input[] = new int[cnt];
+        for(int i = 0; i<cnt; i++){
+            input[i] = sc.nextInt();
+        }
+        int[] dph = new int[cnt];
+        int[] dpl = new int[cnt];
 
-}
+        for(int i = 0; i<cnt; i++){
+            dph[i] = 1;
+            dpl[i] = 1;
+        }//초기화
+
+        for(int i = 1; i<cnt; i++){
+            for(int j = i; j>=0; j--){
+                if(input[i]>input[j]){
+                    dph[i] = getMax(dph[i],dph[j]+1);
+                }//if
+            }//for
+        }//for
+
+        for(int i = cnt-1; i>=0; i--){
+            for(int j = i; j<cnt; j++){
+                if(input[i]>input[j]){
+                    dpl[i] = getMax(dpl[i],dpl[j]+1);
+                }//if
+            }//for
+        }//for
+
+        int max = 0;
+        for(int i = 0; i<cnt; i++){
+            if(max<dph[i]+dpl[i]){
+                max = dph[i]+dpl[i];
+            }
+        }
+        System.out.println(max-1);
+    }//main
+
+}//class

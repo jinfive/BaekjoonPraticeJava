aa820c58f63bc8677b76beb997462be53593264d
Kjo <krt8599@naver.com>
2024-12-30 22:06:18 +0900

    고양이 문제

--- src/main/java/org/example/OnedayCoding/day4/Boj10171.java
+++ src/main/java/org/example/OnedayCoding/day4/Boj10171.java
@@ -1,4 +1,11 @@
 package org.example.OnedayCoding.day4;
 
 public class Boj10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
 }

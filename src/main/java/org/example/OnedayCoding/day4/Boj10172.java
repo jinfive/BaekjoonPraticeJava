84b08eec0d340c54ba5329493d863bad3facc60f
Kjo <krt8599@naver.com>
2024-12-30 22:13:00 +0900

    강아지

--- src/main/java/org/example/OnedayCoding/day4/Boj10172.java
+++ src/main/java/org/example/OnedayCoding/day4/Boj10172.java
@@ -1,4 +1,12 @@
 package org.example.OnedayCoding.day4;
 
 public class Boj10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
 }

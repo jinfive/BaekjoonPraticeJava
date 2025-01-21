0b02eb6b242bcb92f06b1525d25fda45526ad674
Kjo <krt8599@naver.com>
2025-01-21 22:14:40 +0900

    practice 11021

---
+++ src/main/java/org/example/OnedayCoding/day8/Boj11021.java
@@ -0,0 +1,15 @@
+package org.example.OnedayCoding.day8;
+
+import java.util.Scanner;
+
+public class Boj11021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #"+i+": " +(a + b));
+        }
+    }
+}

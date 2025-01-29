236837f075c82a634a1297b7bd71115985fc9787
Kjo <krt8599@naver.com>
2025-01-29 19:31:17 +0900

    practice 18108

---
+++ src/main/java/org/example/OnedayCoding/day11/Boj18108.java
@@ -0,0 +1,12 @@
+package org.example.OnedayCoding.day11;
+
+import java.util.Scanner;
+
+public class Boj18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Byear = scanner.nextInt();
+        scanner.close();
+        System.out.println(Byear-543);
+    }
+}

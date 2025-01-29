513bc72e09bf40dc458421345f03358d01bbc2a4
Kjo <krt8599@naver.com>
2025-01-29 19:19:21 +0900

    pra

---
+++ src/main/java/org/example/OnedayCoding/day10/Boj11654.java
@@ -0,0 +1,13 @@
+package org.example.OnedayCoding.day10;
+
+import java.util.Scanner;
+
+public class Boj11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        scanner.close();
+        char c = s.charAt(0);
+        System.out.println((int)c);
+    }
+}

54e0a70079a806accf21a1b59e4fdabbc78bae8e
Kjo <krt8599@naver.com>
2025-02-06 21:46:43 +0900

    11382 Long

---
+++ src/main/java/org/example/OnedayCoding/day15/Boj11382.java
@@ -0,0 +1,14 @@
+package org.example.OnedayCoding.day15;
+
+import java.util.Scanner;
+
+public class Boj11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Long a= scanner.nextLong();
+        Long b= scanner.nextLong();
+        Long c= scanner.nextLong();
+        System.out.println(a+b+c);
+        scanner.close();
+    }
+}

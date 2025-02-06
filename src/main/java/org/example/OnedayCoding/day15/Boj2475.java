1a6f95c0fcf1136afd1feb652d07e5d3597b9153
Kjo <krt8599@naver.com>
2025-02-06 21:53:56 +0900

    제곱

---
+++ src/main/java/org/example/OnedayCoding/day15/Boj2475.java
@@ -0,0 +1,16 @@
+package org.example.OnedayCoding.day15;
+
+import java.util.Scanner;
+
+public class Boj2475 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = scanner.nextInt();
+            sum += a*a;
+        }
+        scanner.close();
+        System.out.println(sum%10);
+    }
+}

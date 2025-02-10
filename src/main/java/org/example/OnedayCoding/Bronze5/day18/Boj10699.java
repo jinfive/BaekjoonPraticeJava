0b054c36943114d12093483603eb935438950673
Kjo <krt8599@naver.com>
2025-02-10 22:55:12 +0900

    p today

---
+++ src/main/java/org/example/OnedayCoding/Bronze5/day18/Boj10699.java
@@ -0,0 +1,10 @@
+package org.example.OnedayCoding.Bronze5.day18;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Boj10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}

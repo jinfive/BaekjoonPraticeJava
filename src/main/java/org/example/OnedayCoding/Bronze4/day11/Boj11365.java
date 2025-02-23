2ce3f252a94b442c4fabde6aaf08779045d33a1a
Kjo <krt8599@naver.com>
2025-02-23 21:28:18 +0900

    ㅔ 11365

--- src/main/java/org/example/OnedayCoding/Bronze4/day11/Boj11365.java
+++ src/main/java/org/example/OnedayCoding/Bronze4/day11/Boj11365.java
@@ -1,4 +1,21 @@
 package org.example.OnedayCoding.Bronze4.day11;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Boj11365 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            String s = br.readLine();
+            if(s.equals("END"))break;
+            char[] chars = s.toCharArray();
+            for (int i = chars.length -1; i >=0 ; i--){
+                System.out.print(chars[i]);
+            }
+            System.out.println();
+        }
+        br.close();
+    }
 }

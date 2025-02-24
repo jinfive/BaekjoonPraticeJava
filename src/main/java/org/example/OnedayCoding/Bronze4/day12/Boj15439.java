0506a27f6e3fca53c79fe1b1ac85792e22fbfc7b
Kjo <krt8599@naver.com>
2025-02-24 22:18:52 +0900

    Boj15439

---
+++ src/main/java/org/example/OnedayCoding/Bronze4/day12/Boj15439.java
@@ -0,0 +1,12 @@
+package org.example.OnedayCoding.Bronze4.day12;
+
+import java.util.Scanner;
+
+public class Boj15439 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n * (n-1));
+        scanner.close();
+    }
+}

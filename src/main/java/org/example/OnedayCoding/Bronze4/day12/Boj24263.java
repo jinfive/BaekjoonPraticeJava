30815138e7cfb3f9919f0f44d6d6250b36ba9710
Kjo <krt8599@naver.com>
2025-02-24 22:23:53 +0900

    practice Boj24263

---
+++ src/main/java/org/example/OnedayCoding/Bronze4/day12/Boj24263.java
@@ -0,0 +1,14 @@
+package org.example.OnedayCoding.Bronze4.day12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+        System.out.println(1);
+    }
+}

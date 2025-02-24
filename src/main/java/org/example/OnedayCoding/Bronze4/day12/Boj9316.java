d2e39cbd5abb4b730a259ec09b7f5aa75bb22f52
Kjo <krt8599@naver.com>
2025-02-24 22:13:30 +0900

    practice Boj9316

---
+++ src/main/java/org/example/OnedayCoding/Bronze4/day12/Boj9316.java
@@ -0,0 +1,13 @@
+package org.example.OnedayCoding.Bronze4.day12;
+
+import java.util.Scanner;
+
+public class Boj9316 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Hello World, Judge "+ i+ "!");
+        }
+    }
+}

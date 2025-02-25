55456e383af96ea1f40b63d2e8948b28655d6334
Kjo <krt8599@naver.com>
2025-02-25 18:32:24 +0900

    p 10786

---
+++ src/main/java/org/example/OnedayCoding/Bronze4/day13/Boj10768.java
@@ -0,0 +1,21 @@
+package org.example.OnedayCoding.Bronze4.day13;
+
+import java.util.Scanner;
+
+public class Boj10768 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+
+        if (month < 2 || (month == 2 && day < 18)) {
+            System.out.println("Before");
+        } else if (month > 2 || (month == 2 && day > 18)) {
+            System.out.println("After");
+        } else {
+            System.out.println("Special");
+        }
+
+}
+

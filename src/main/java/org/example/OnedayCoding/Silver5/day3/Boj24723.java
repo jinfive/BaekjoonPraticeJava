f7983c1bd16058fde2772460fded14c0aff559b2
Kjo <krt8599@naver.com>
2025-02-27 22:05:55 +0900

    Boj24723

---
+++ src/main/java/org/example/OnedayCoding/Silver5/day3/Boj24723.java
@@ -0,0 +1,12 @@
+package org.example.OnedayCoding.Silver5.day3;
+
+import java.util.Scanner;
+
+public class Boj24723 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long result = (long) Math.pow(2,n);
+        System.out.println(result);
+    }
+}

7c2cbacb2b9c332029da0155552638583c8206f8
jin_5 <krt8599@naver.com>
2025-03-15 19:05:02 +0900

    practice11719

---
+++ src/main/java/org/example/OnedayCoding/Bronze3/day8/Boj11719.java
@@ -0,0 +1,15 @@
+package org.example.OnedayCoding.Bronze3.day8;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Boj11719 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+
+    }
+}

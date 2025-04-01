f96fce628c15affccfef4ff33431736e1e1d38b0
jin_5 <krt8599@naver.com>
2025-04-01 20:35:05 +0900

    practice 3052

---
+++ src/main/java/org/example/OnedayCoding/Bronze2/day1/Boj3052.java
@@ -0,0 +1,33 @@
+package org.example.OnedayCoding.Bronze2.day1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj3052 {
+
+
+    // The main method must be in a class named "Main".
+
+        public static void main (String[] args)  throws Exception{
+            int[] arr = new int[10];
+
+            boolean[] check = new boolean[43];
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            for(int i = 0 ; i < 10 ; i++){
+                arr[i] = Integer.parseInt(br.readLine());
+                //System.out.println(arr[i]);
+                check[arr[i] % 42] = true;
+            }
+            int cnt = 0;
+            for(int i = 0 ; i < check.length ; i++){
+                if(check[i]){
+                    cnt++;
+                }
+            }
+            System.out.print(cnt);
+
+
+        }
+
+    
+}

40a00b7c407405857d581b82187046b81368c908
jin_5 <krt8599@naver.com>
2025-04-01 21:44:07 +0900

    practice 1152

---
+++ src/main/java/org/example/OnedayCoding/Bronze2/day1/Boj1152.java
@@ -0,0 +1,28 @@
+package org.example.OnedayCoding.Bronze2.day1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Boj1152 {
+
+
+    // The main method must be in a class named "Main".
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int count = 0;
+        List<String> list = Arrays.asList(s.split(" "));
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).isEmpty()) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+
+    }
+}
--- src/main/java/org/example/OnedayCoding/Bronze2/day1/Boj3052.java
+++ src/main/java/org/example/OnedayCoding/Bronze2/day1/Boj3052.java
@@ -29,5 +29,5 @@ public class Boj3052 {
 
         }
 
-    
+
 }

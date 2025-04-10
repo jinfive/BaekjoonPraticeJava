9b8268bd5f2ca4d2d5404d3a8d0d5be30784d0eb
jin_5 <krt8599@naver.com>
2025-04-10 22:50:02 +0900

    practice 2587

---
+++ src/main/java/org/example/OnedayCoding/Bronze2/day10/Boj2587.java
@@ -0,0 +1,23 @@
+package org.example.OnedayCoding.Bronze2.day10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj2587 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] A = new int[5];
+        int sum = 0;
+        for(int i = 0 ; i < 5 ; i++){
+            A[i] = Integer.parseInt(br.readLine());
+            sum += A[i];
+        }
+        Arrays.sort(A);
+        System.out.println(sum/5);
+        System.out.println(A[2]);
+
+
+
+
+    }

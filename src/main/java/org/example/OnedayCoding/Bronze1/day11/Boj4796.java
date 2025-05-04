5211711d77621a9010543bc4398a59453328acb8
jin_5 <krt8599@naver.com>
2025-05-04 18:22:59 +0900

    practice 13023

--- src/main/java/org/example/OnedayCoding/Bronze1/day11/Boj4796.java
+++ src/main/java/org/example/OnedayCoding/Bronze1/day11/Boj4796.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj4796 {
-  
+
     class Main {
         public static void main(String[] args) throws Exception{
             StringBuilder sb = new StringBuilder();
---
+++ src/main/java/org/example/OnedayCoding/Bronze1/day11/practice2023.java
@@ -0,0 +1,46 @@
+package org.example.OnedayCoding.Bronze1.day11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class practice2023 {
+ 
+    class Main {
+        private static StringBuilder sb = new StringBuilder();
+        private static int num;
+        public static void main(String[] args) throws Exception{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            num = Integer.parseInt(br.readLine());
+            DFS(2,1);
+            DFS(3,1);
+            DFS(5,1);
+            DFS(7,1);
+            System.out.println(sb.toString());
+
+        }
+        private static void DFS(int n , int c){
+            if( c == num){
+                if(isPrime(n)){
+                    sb.append(n +"\n");
+                }
+                return;
+            }
+            else if(isPrime(n)){
+                for(int i = 1; i < 10 ;i++){
+                    if(i % 2 ==0) continue;
+                    DFS(n * 10 + i , c +1);
+                }
+
+            }
+        }
+        private static boolean isPrime(int n){
+            for(int i = 2 ; i <= n/2 ; i++){
+                if(n % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}

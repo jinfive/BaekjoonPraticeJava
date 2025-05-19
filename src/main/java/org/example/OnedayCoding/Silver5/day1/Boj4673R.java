d0d9d389a01090303b212be2fb9382d9f3dcbfb3
jin_5 <krt8599@naver.com>
2025-05-19 12:06:21 +0900

    practice 4673

---
+++ src/main/java/org/example/OnedayCoding/Silver5/day1/Boj4673R.java
@@ -0,0 +1,30 @@
+package org.example.OnedayCoding.Silver5.day1;
+
+public class Boj4673R {
+ 
+    // The main method must be in a class named "Main".
+    class Main {
+        private static boolean[] check;
+
+        private static int answer = 0;
+        public static void main(String[] args) throws Exception{
+            check = new boolean[10001];
+            for(int i = 1 ; i < 10001 ; i++){
+                int num = i;
+                String nStr = i +"";
+                for(int j = 0 ; j < nStr.length() ; j++){
+                    num += (int)(nStr.charAt(j)-'0');
+
+                }
+                if(num < 10001){
+                    check[num] = true;
+                }
+            }
+            for(int i = 1 ; i < check.length ; i++){
+                if(!check[i]){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}

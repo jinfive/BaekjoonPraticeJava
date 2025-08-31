812905dde429b9ff2c7197ca65acdb47223969ce
jin_5 <krt8599@naver.com>
2025-08-31 19:59:46 +0900

    practice 12869

---
+++ src/main/java/org/example/OnedayCoding/Silver3/day15/Boj12869.java
@@ -0,0 +1,35 @@
+package org.example.OnedayCoding.Silver3.day15;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj12869 {
+    
+
+    // The main method must be in a class named "Main".
+    class Main {
+        public static void main(String[] args) throws Exception{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            String str = br.readLine();
+            char[] arr = str.toCharArray();
+            int answer = 0;
+            for(int i = 0 ; i < n ; i ++){
+                if(arr[i] == 'P'){
+                    for(int j = Math.max(0 , i - k) ; j <= Math.min(n - 1, i + k) ; j ++){
+                        if(arr[j] == 'H'){
+                            arr[j] ='O';
+                            answer++;
+                            break;
+                        }
+                    }
+                }
+            }
+            System.out.println(answer);
+
+        }
+    }
+}
